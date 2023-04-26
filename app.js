78140c02ee26dd7f8d9c8a214ede1e1b0da2afc5
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-04-26 02:51:41 -0700

    allows multiple ends

--- app.js
+++ app.js
@@ -12,11 +12,22 @@ function direction(from, to) {
 
 app.get('/directions', (req, res) => {
   const [lat1, lon1] = req.query.start.split(',').map(parseFloat);
-  const [lat2, lon2] = req.query.end.split(',').map(parseFloat);
+  const nEnd = parseInt(req.query.nend ?? 1);
+  const ends = [...new Array(nEnd)]
+    .map((_, i) => req.query['end' + i].split(',').map(parseFloat));
 
-  fetch(direction([lon1, lat1], [lon2, lat2]))
-    .then(response => response.json())
-    .then(jsonData => res.json(jsonData));
+  if (!ends.every(d => d.length === 2 && !Number.isNaN(d[0]) && !Number.isNaN(d[1]))) {
+    throw new Error('invalid ends: ' + ends.toString());
+  }
+
+  Promise.all([...new Array(nEnd)]
+    .map((_, i) => req.query['end' + i].split(',').map(parseFloat))
+    .map(([lon2, lat2]) => 
+      fetch(direction([lon1, lat1], [lon2, lat2]))
+        .then(response => response.json())
+    )
+  ).then(jsons => res.send(JSON.stringify(jsons)));
+
 })
 
 app.listen(port, () => {
